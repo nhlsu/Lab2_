9528112ee9dc7d36bc4e0016c0eadbaf2b6c2ae7
nhlsu <nadiahalbinali.com>
2025-01-27 15:40:40 +0300

    second added

--- lab2.java
+++ lab2.java
@@ -1,2 +1,6 @@
 public static void main(String[] args){
-System.out.println("Nadiah Albinali says Hello world1");}
+System.out.println("Nadiah Albinali says Hello world1");
+System.out.println("Its a wonderful day");
+System.out.println("Third times the charm");
+}
+
