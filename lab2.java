d507c0b5b3069bf62e3bda806e33500b1208fe73
nhlsu <nadiahalbinali.com>
2025-01-28 14:41:57 +0300

    before submission

--- lab2.java
+++ lab2.java
@@ -1,7 +1,7 @@
 public static void main(String[] args){
 System.out.println("Nadiah Albinali says Hello world1");
 System.out.println("Its a wonderful day");
-System.out.println("Third times the charm11");
+System.out.println("Third times the charm");
 
 }
 
